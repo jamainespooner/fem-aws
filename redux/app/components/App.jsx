fc5abd6768daedd79b9e0f0e46402921a5fb265d
Jamaine Spooner <jamaine@jamaines-mbp.home>
2019-02-27 21:17:27 +0000

    added not found

--- redux/app/components/App.jsx
+++ redux/app/components/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 
 const App = ({ store }) =>
   <Provider store={store}>
     <Router>
-      <div>
-        <Route path="/" exact component={Home} />
-        <Route path="/notes" render={() => <div>notes</div>} />
-        <Route path="/contact" render={() => <div>contact</div>} />
-      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/notes" render={() => <div>empty notes</div>} />
+        <Route path="/notes/:id" render={() => <div>notes</div>} />
+        <Route render={() => <div>not found</div>} />
+      </Switch>
     </Router>
   </Provider>
 
